439fc64075d799420a0d6069c1b46d0c5f26d4e3
Brian Kim <brian2562@gmail.com>
2024-12-31 21:29:43 +0900

    ✨add MyLikesPage

--- src/pages/MyLikesPage.tsx
+++ src/pages/MyLikesPage.tsx
@@ -1,9 +1,27 @@
 /*
   나의 관심목록 페이지.
 */
+import { NavLink } from 'react-router-dom';
+
+import leftArrow from '../assets/leftarrow.svg';
+import Item from '../components/Item';
+import styles from '../css/MyLikesPage.module.css';
 
 const MyLikesPage = () => {
-  return <div>내 관심목록</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.upperBar}>
+        <NavLink to="/mypage">
+          <img src={leftArrow} className={styles.upperIcon} />
+        </NavLink>
+        <p className={styles.pageTitle}>관심목록</p>
+      </div>
+      <div>
+        <Item />
+        <Item />
+      </div>
+    </div>
+  );
 };
 
 export default MyLikesPage;
