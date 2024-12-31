9833d835db4b992d7a01802eadbed666c7b3bb4a
Quad72 <quad.heo@snu.ac.kr>
2024-12-31 13:21:44 +0900

    :bug: fix typos of ItemPostPage

--- src/pages/ItemPostPage.tsx
+++ src/pages/ItemPostPage.tsx
@@ -10,7 +10,7 @@ import styles from '../css/ItemPostPage.module.css';
 
 const LONG_PLACEHOLDER_TEXT = `에 올릴 게시글 내용을 작성해 주세요. (판매 금지 물품은 게시가 제한될 수 있어요.)
 
-신뢰할 수 있는 거래를 위해 자세히 적어주세요. 과학기술정보통신부, 한국 인터넥진흥원과 함께 해요.`;
+신뢰할 수 있는 거래를 위해 자세히 적어주세요. 과학기술정보통신부, 한국 인터넷진흥원과 함께 해요.`;
 
 const ItemPostPage = () => {
   const [title, setTitle] = useState<string>('');
