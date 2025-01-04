3854d8c8d89437f1824d1b89aa1ad155f3d8ab06
Brian Kim <brian2562@gmail.com>
2025-01-04 18:59:36 +0900

    🐛prettier fix

--- src/typings/communityPost.ts
+++ src/typings/communityPost.ts
@@ -1,15 +1,15 @@
 export type CommunityPost = {
-  id: string;       // 게시글 id
+  id: string; // 게시글 id
   tag: string;
   title: string;
   body: string;
   user_id: string;
   nickname: string;
   location: string;
-  time: string;     // 2025-01-02T13:05:26 이런 형식인듯?
-  views: number;    // 조회수
+  time: string; // 2025-01-02T13:05:26 이런 형식인듯?
+  views: number; // 조회수
   likes: number;
-  comments: Comment[];    // 댓글에 답글은 없는걸로
+  comments: Comment[]; // 댓글에 답글은 없는걸로
 };
 
 type Comment = {
@@ -17,5 +17,5 @@ type Comment = {
   location: string;
   time: string;
   likes: number;
-  body: string;       // 댓글 내용
+  body: string; // 댓글 내용
 };
