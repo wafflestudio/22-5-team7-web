d398a7c12ebf8f45ef7cf70ae690fe6e28c07a43
Brian Kim <brian2562@gmail.com>
2025-01-05 17:22:56 +0900

    ✨add SearchPage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,9 +3,47 @@
   검색창이 메인이고, 추천 검색은 필요 없음.
   필요하면 최근 검색은 localstorage에 저장해서 사용하는 방식으로
 */
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import leftArrow from '../assets/leftarrow.svg';
+import styles from '../css/SearchPage.module.css';
+
+const tempLocation = '대학동';
 
 const SearchPage = () => {
-  return <div>검색창</div>;
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (query.trim() !== '') void navigate(encodeURIComponent(query.trim()));
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.upperBar}>
+        <NavLink to="/main">
+          <img src={leftArrow} className={styles.upperIcon} />
+        </NavLink>
+        <input
+          type="search"
+          className={styles.searchBar}
+          placeholder={`${tempLocation} 근처에서 검색`}
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') handleSearch();
+          }}
+        />
+        <NavLink to="/main" className={styles.closeButton}>
+          닫기
+        </NavLink>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default SearchPage;
